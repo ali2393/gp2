0a91e6a8c9721354cfe26915ee092e98883cd834
Alisdair Muircroft <alisdairmuircroft@hotmail.com>
2012-10-04 08:34:22 +0000

    IndexBufferFixed

--- GP2/GameApplication.cpp
+++ GP2/GameApplication.cpp
@@ -112,7 +112,7 @@ bool CGameApplication::initGame()
 
 	D3D10_BUFFER_DESC indexBufferDesc;
 	indexBufferDesc.Usage=D3D10_USAGE_DEFAULT;
-	indexBufferDesc.ByteWidth=sizeof(indices)*36;
+	indexBufferDesc.ByteWidth=sizeof(int)*36;
 	indexBufferDesc.BindFlags=D3D10_BIND_INDEX_BUFFER;
 	indexBufferDesc.CPUAccessFlags=0;
 	indexBufferDesc.MiscFlags=0;
